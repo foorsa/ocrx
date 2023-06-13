824c4bb31b6a7c00fbc9023a7efd250758dedf32
YASSINE CHETTOUCH <yassine_ct@pm.me>
2023-06-13 23:04:20 +0100

    Exported Types.

--- client/redux/store.ts
+++ client/redux/store.ts
@@ -1,24 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import thunk from 'redux-thunk';
 
-// Define the initial state of our store
+// Define the initial state of our store (Type)
+
+// Enum properties for the Step
 enum Steps {
-    Upload = 'Upload',
-    Translate = 'Translate',
-    Validate = 'Validate',
+    Upload = "Upload",
+    Translate = "Translate",
+    Validate = "Validate"
 }
 
-// Export
 export { Steps };
 
-interface IState {
+export interface IState {
     File: File | null;
     DocType: string | null;
     Step: Steps;
 }
 
+// Define initial state
 const initialState: IState = {
     File: null,
     DocType: null,
@@ -28,20 +31,20 @@ const initialState: IState = {
 // Define reducer function
 function rootReducer(state = initialState, action: { type: string; payload: any }) {
     switch (action.type) {
-        case 'SET_FILE':
+        case "SET_FILE":
             return {
                 ...state,
-                File: action.payload,
+                File: action.payload
             };
-        case 'SET_DOCTYPE':
+        case "SET_DOCTYPE":
             return {
                 ...state,
-                DocType: action.payload,
+                DocType: action.payload
             };
-        case 'SET_STEP':
+        case "SET_STEP":
             return {
                 ...state,
-                Step: action.payload,
+                Step: action.payload
             };
         default:
             return state;
@@ -59,9 +62,21 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 // Create the Redux store
 export const store = configureStore({
     reducer: persistedReducer,
-    devTools: process.env.NODE_ENV !== 'production',
+    devTools: process.env.NODE_ENV !== "production",
     middleware: [thunk],
 });
 
 // Initialize Redux Persist
 export const persistor = persistStore(store);
+
+// Export the store and persistor
+export default { store, persistor };
+
+// Root State 
+export type RootState = ReturnType<typeof store.getState>;
+
+// Root Dispatch
+export type RootDispatch = ReturnType<typeof store.dispatch>;
+
+// App Dispatch
+export type AppDispatch = typeof store.dispatch;
