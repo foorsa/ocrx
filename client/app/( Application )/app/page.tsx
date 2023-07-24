457883620296deeffe83f0e88acf9ad1fd114df9
eldevyas <yassinechettouch@gmail.com>
2023-07-24 02:03:12 +0100

    Badges to Unavailable Docs.

--- client/app/( Application )/app/page.tsx
+++ client/app/( Application )/app/page.tsx
@@ -142,6 +142,18 @@ export default function Page() {
                                             >
                                                 {document.description}
                                             </p>
+
+                                            {/* Badges Footer */}
+                                            <div className="relative flex flex-row items-center justify-start gap-2 w-full mt-5">
+                                                {!!document.tags &&
+                                                    document.tags.map((tag) => {
+                                                        return (
+                                                            <span className="bg-rose-100 text-rose-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-zinc-900 dark:text-zinc-400 border border-rose-400 dark:border-zinc-400 group-hover:bg-white group-hover:text-rose-600 group-hover:border-white">
+                                                                {tag}
+                                                            </span>
+                                                        );
+                                                    })}
+                                            </div>
                                         </div>
                                     )}
                                 </>
