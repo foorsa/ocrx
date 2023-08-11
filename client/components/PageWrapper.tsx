63e2a99a931f7e20ff8fb1c89ca0421b94b65e6f
eldevyas <yassinechettouch@gmail.com>
2023-08-11 23:57:56 +0100

    Motion shared layout animations work-around.

--- client/components/PageWrapper.tsx
+++ client/components/PageWrapper.tsx
@@ -2,41 +2,54 @@
 
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter, usePathname } from "next/navigation";
-import React from "react";
+import { PropsWithChildren, useRef, useContext } from "react";
+import { LayoutRouterContext } from "next/dist/shared/lib/app-router-context";
 
 export default function Page({ children }: { children: React.ReactNode }) {
     const Pathname = usePathname();
 
     return (
-        // <AnimatePresence mode="wait">
-        <motion.div
-            initial="initial"
-            animate="animate"
-            // exit="exit"
-            key={Pathname}
-            variants={{
-                initial: {
-                    opacity: 0,
-                    y: 15,
-                },
-                animate: {
-                    opacity: 1,
-                    y: 0,
-                },
-                exit: {
-                    opacity: 0,
-                    y: 15,
-                },
-            }}
-            transition={{
-                type: "spring",
-                stiffness: 260,
-                damping: 20,
-            }}
-            className="h-full min-w-full w-full flex flex-col justify-between items-center min-h-screen"
-        >
-            {children}
-        </motion.div>
-        // </AnimatePresence>
+        <AnimatePresence mode="wait">
+            <motion.div
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                key={Pathname}
+                variants={{
+                    initial: {
+                        opacity: 0,
+                        y: 15,
+                    },
+                    animate: {
+                        opacity: 1,
+                        y: 0,
+                    },
+                    exit: {
+                        opacity: 0,
+                        y: 15,
+                    },
+                }}
+                transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 20,
+                }}
+                className="h-full min-w-full w-full flex flex-col justify-between items-center min-h-screen"
+            >
+                <FrozenRouter>{children}</FrozenRouter>
+            </motion.div>
+        </AnimatePresence>
+    );
+}
+
+/// layout.tsx
+function FrozenRouter(props: PropsWithChildren<{}>) {
+    const context = useContext(LayoutRouterContext);
+    const frozen = useRef(context).current;
+
+    return (
+        <LayoutRouterContext.Provider value={frozen}>
+            {props.children}
+        </LayoutRouterContext.Provider>
     );
 }
