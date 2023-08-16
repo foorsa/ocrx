de13af5f725e11705b74a246390264eeda1cad15
eldevyas <yassinechettouch@gmail.com>
2023-08-17 00:17:02 +0100

    Fixed Background.

--- client/app/Components/A. Background.tsx
+++ client/app/Components/A. Background.tsx
@@ -22,7 +22,7 @@ export default function Background({ Variant }: Props) {
                     <Particles />
                     {/* Bottom Center - Wide blue Gradient */}
                     {/* Division for Blurry Elements */}
-                    <div className="absolute h-full w-full min-h-screen top-0 left-0 right-0 bottom-0 -z-50 opacity-60 dark:opacity-100 overflow-hidden">
+                    <div className="absolute h-full w-full min-h-screen top-0 left-0 right-0 bottom-0 -z-50 opacity-60 dark:opacity-60 overflow-hidden max-h-screen">
                         <div className="absolute w-3/5 h-3/5 bottom-0 left-1/2 -translate-x-1/2 rounded-full blur-3xl bg-gradient-to-b from-transparent via-blue-800 to-blue-400 -z-40 dark:from-transparent dark:via-blue-800 dark:to-blue-400"></div>
 
                         {/* Bottom Center - Light blue */}
@@ -40,7 +40,7 @@ export default function Background({ Variant }: Props) {
                 </>
             ) : (
                 <>
-                    <div className="absolute h-full w-full min-h-full top-0 left-0 right-0 bottom-0 -z-50 opacity-60 dark:opacity-100 max-h-screen">
+                    <div className="absolute h-full w-full min-h-screen top-0 left-0 right-0 bottom-0 -z-50 opacity-60 dark:opacity-100 max-h-screen">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="1440"
