7b13195e602c56a573270356b571b4aee023b16a
YASSINE CHETTOUCH <yassinechettouch@gmail.com>
2024-02-23 18:53:03 +0100

    Add authentication check and redirect to sign-in page if session is not available

--- client/app/( Application )/app/translate/page.tsx
+++ client/app/( Application )/app/translate/page.tsx
@@ -1,13 +1,25 @@
 import AppHeader from "./Layout/A. App Header";
 import Container from "./Application/Container";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+import { redirect } from "next/navigation";
+import LoadingPage from "@/app/loading";
 
-export default function TranslatePage({
+export default async function TranslatePage({
 	searchParams,
 }: {
 	searchParams: { [key: string]: string | string[] | undefined };
 }) {
 	// Query
 	const QueryDOC = searchParams.doc;
+
+	const session = await getServerSession(authOptions);
+
+	if (!session) {
+		redirect("/auth/sign-in");
+
+		return <LoadingPage />;
+	}
 
 	return <Container DocId={QueryDOC} />;
 }
