d634d3ebb65d5d64b8d2d61261977059b1e478d9
YASSINE CHETTOUCH <yassinechettouch@gmail.com>
2024-05-25 18:55:37 +0100

    Update documents route to use Next.js API routes

--- client/app/api/documents/route.ts
+++ client/app/api/documents/route.ts
@@ -1,9 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
 import { Documents } from "@/redux/data/Documents";
-
-export async function GET(Request: NextRequest) {
-	return NextResponse.json(Documents);
-}
+import { NextApiRequest, NextApiResponse } from "next";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 60; // 1 minute
@@ -11,3 +7,17 @@ export const fetchCache = "auto";
 export const runtime = "nodejs";
 export const preferredRegion = "auto";
 export const maxDuration = 60 * 1000;
+
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+	return new Response(JSON.stringify(Documents), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+			"Cache-Control": "no-store, max-age=0",
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET",
+			"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Max-Age": "86400",
+		},
+	});
+}
